52d284ef8f913fde1b34e73abf347f7b7ae86e28
buhuiming <873247376@qq.com>
2018-06-19 08:58:04 +0800

    webView

--- BHMLibrary/src/main/java/com/bhm/sdk/bhmlibrary/interfaces/WebViewCallBack.java
+++ BHMLibrary/src/main/java/com/bhm/sdk/bhmlibrary/interfaces/WebViewCallBack.java
@@ -1,22 +1,41 @@
 package com.bhm.sdk.bhmlibrary.interfaces;
 
+import android.graphics.Bitmap;
 import android.webkit.WebView;
 
 /**
  * Created by bhm on 2018/3/28.
  */
 
-public interface WebViewCallBack {
+public class WebViewCallBack extends WebViewCallBackImp{
 
-    boolean shouldOverrideUrlLoading(WebView view, String url);//一般返回true,自己处理url跳转
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        return false;
+    }
 
-    void onPageStarted(WebView view, String url, android.graphics.Bitmap favicon);
+    @Override
+    public void onPageStarted(WebView view, String url, Bitmap favicon) {
 
-    void onPageFinished(WebView view, String url);
+    }
 
-    void onLoadResource(WebView view, String url);
+    @Override
+    public void onPageFinished(WebView view, String url) {
 
-    void onReceivedError(WebView view, int errorCode, String description, String failingUrl);
+    }
 
-    void onReceivedTitle(WebView view, String title);
+    @Override
+    public void onLoadResource(WebView view, String url) {
+
+    }
+
+    @Override
+    public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+
+    }
+
+    @Override
+    public void onReceivedTitle(WebView view, String title) {
+
+    }
 }
