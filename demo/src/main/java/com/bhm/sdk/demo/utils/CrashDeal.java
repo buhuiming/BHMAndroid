07d7fcb1db23475e2ed5ede09c07cf8bacaca92d
buhuiming <873247376@qq.com>
2018-06-27 09:07:51 +0800

    crash

--- demo/src/main/java/com/bhm/sdk/demo/utils/CrashDeal.java
+++ demo/src/main/java/com/bhm/sdk/demo/utils/CrashDeal.java
@@ -14,6 +14,9 @@ public class CrashDeal implements ExceptionHandler {
 
     @Override
     public void handlerException(Thread thread, Throwable throwable) {
+        //集成此机制，防止APP因为抛异常闪退或者宕机。
+        //所有未有try catch处理的异常，统一在此处理。可以给出提示，重启APP。
+        //或者上传异常日志到服务器
         String errorMsg = CrashManager.getStackMsg(throwable);
         Toast.makeText(BaseApplication.getApplication(), errorMsg, Toast.LENGTH_LONG).show();
     }
